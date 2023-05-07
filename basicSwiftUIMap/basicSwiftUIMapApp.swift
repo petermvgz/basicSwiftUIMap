ff88ba419a8e149e8e9d10797339af7437cbe1a9
Peter Deyi <peterdeyi@icloud.com>
2023-05-06 20:45:05 -0700

    Initial Commit

---
+++ basicSwiftUIMap/basicSwiftUIMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicSwiftUIMapApp.swift
+//  basicSwiftUIMap
+//
+//  Created by Peter Deyi on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct basicSwiftUIMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
